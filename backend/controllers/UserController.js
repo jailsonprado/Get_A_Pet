6bfd33aea5cdf707ba4ddd06ebcb286a9a158d56
Jailson Prado <jailsondpd@gmail.com>
2021-11-14 17:02:24 -0300

    Completando autenticacao JWT

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,6 +1,7 @@
 const User = require('../models/User');
 
 const bcrypt = require('bcrypt');
+const createUserToken = require('../helpers/create-user-token');
 
 module.exports = class UserController{
     static async register(req, res){
@@ -59,10 +60,9 @@ module.exports = class UserController{
 
       try {
           const newUser = await user.save()
-          res.status(201).json({
-            message: 'Usuario criado com sucesso!',
-            newUser,
-        })   
+
+          await createUserToken(newUser, req, res)
+      
       } catch (error) {
           res.status(500).json({
               message: error})
--- backend/helpers/create-user-token.js
+++ backend/helpers/create-user-token.js
@@ -6,13 +6,13 @@ const createUserToken = async (user, req, res) => {
     const token = jwt.sign({
         name: user.name,
         id: user._id
-    }, "nossosecret")
+    }, 'nossosecret')
 
     // return token
     res.status(200).json({
         message: "Você está autenticado!",
         token: token,
-        userId: user_id,
+        userId: user._id,
     })
 }
 module.exports = createUserToken
