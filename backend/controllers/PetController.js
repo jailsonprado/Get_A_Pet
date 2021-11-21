f780ebd5c3cd961c18c846220ce5ebfd5ecaa848
Jailson Prado <jailsondpd@gmail.com>
2021-11-21 16:11:52 -0300

    Method findOne Pets By ID

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -3,6 +3,7 @@ const Pet = require("../models/Pet");
 // Helpers
 const getToken = require("../helpers/get-token");
 const getUserByToken = require("../helpers/get-user-by-token");
+const ObjectId = require('mongoose').Types.ObjectId;
 
 module.exports = class PetController {
   // Create a pet
@@ -90,4 +91,40 @@ module.exports = class PetController {
         pets,
     })
   }
+
+  // Method adoptions pets
+  static async getAllUserAdoptions(req, res){
+     // get user from token
+    const token = getToken(req);
+    const user = await getUserByToken(token);
+
+    const pets = await Pet.find({'adopter._id': user._id}).sort('-createdAt')
+
+    res.status(200).json({
+        pets,
+    })
+  }
+
+  // Create method to find object Pet by ID
+  static async getPetById(req, res){
+      const id = req.params.id
+
+      if(!ObjectId.isValid(id)){
+          res.status(422).json({message: 'ID Invalido'})
+          return;
+      }
+
+      // Check if pet exists
+      const pet = await Pet.findOne({_id: id})
+
+      if(!pet){
+          res.status(404).json({message: 'Pet não encontrado!'})
+      }
+
+      res.status(200).json({
+          pet: pet,
+      })
+  }
+
+    
 };
